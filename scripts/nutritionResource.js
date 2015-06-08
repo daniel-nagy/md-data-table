52d0bb2831ab62d74e0b75319684591ffe990238
Daniel Nagy <Danny@Dannys-MacBook-Pro.local>
2015-06-07 20:37:31 -0400

    I copied and pasted my spelling error

--- scripts/nutritionResource.js
+++ scripts/nutritionResource.js
@@ -1,7 +1,7 @@
 angular.module('nutritionApp').factory('$nutrition', ['$resource', function ($resource) {
   'use strict';
   
-  var desserts = $resource('https://infinite-earth-4803.herokuapp.com/nutriton/deserts', {filter: 'name', limit: 10, page: 1});
+  var desserts = $resource('https://infinite-earth-4803.herokuapp.com/nutriton/desserts', {filter: 'name', limit: 10, page: 1});
   
   return {
     desserts: desserts

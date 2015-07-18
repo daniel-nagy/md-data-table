df128cc60c2cd303b26f3b4cf2d00894aee3273a
vcastello <castellofos@gmail.com>
2015-07-18 20:38:54 +0200

    add new navigation buttons (first and last)

--- app/md-data-table/scripts/mdDataTablePagination.js
+++ app/md-data-table/scripts/mdDataTablePagination.js
@@ -2,20 +2,20 @@ angular.module('md.data.table')
 
 .directive('mdDataTablePagination', ['$q', function ($q) {
   'use strict';
-  
+
   function postLink(scope, element, attrs) {
     scope.paginationLabel = {
       text: 'Rows per page:',
       of: 'of'
     };
-    
+
     if(angular.isObject(scope.label)) {
       angular.extend(scope.paginationLabel, scope.label);
     }
-    
+
     // table progress
     if(angular.isFunction(scope.trigger)) {
-      
+
       // the pagination directive is outside the table directive so we need
       // to locate the controller
       var findTable = function(parent, callback) {
@@ -27,20 +27,20 @@ angular.module('md.data.table')
         }
         callback(angular.element(parent.firstElementChild));
       };
-      
+
       var setTrigger = function(table) {
         var tableCtrl = table.controller('mdDataTable');
-        
+
         if(!tableCtrl) {
           return console.warn('Table Pagination: Could not locate your table directive, your ' + attrs.mdTrigger + ' function will not work.');
         }
-        
+
         scope.pullTrigger = function () {
           var deferred = tableCtrl.defer();
           $q.when(scope.trigger(scope.page, scope.limit))['finally'](deferred.resolve);
         };
       };
-      
+
       findTable(element.parent()[0], setTrigger);
     }
   }
@@ -62,55 +62,76 @@ angular.module('md.data.table')
 
 .controller('mdPaginationCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
   'use strict';
-  
+
   var min = 1;
-  
+
   $scope.hasNext = function () {
     return (($scope.page * $scope.limit) < $scope.total);
   };
-  
+
   $scope.hasPrevious = function () {
     return ($scope.page > 1);
   };
-  
+
   $scope.next = function () {
     $scope.page++;
-    
+
     if($scope.pullTrigger) {
       $timeout($scope.pullTrigger);
     }
-    
+
     min = $scope.min();
   };
-  
+
+  $scope.last = function () {
+    $scope.page = Math.ceil($scope.total / $scope.limit);
+
+    if($scope.pullTrigger) {
+      $timeout($scope.pullTrigger);
+    }
+
+    min = $scope.min();
+  };
+
   $scope.min = function () {
     return ((($scope.page - 1) * $scope.limit) + 1);
   };
-  
+
   $scope.max = function () {
     return $scope.hasNext() ? $scope.page * $scope.limit : $scope.total;
   };
-  
+
   $scope.onSelect = function () {
     $scope.page = Math.floor(min / $scope.limit) + 1;
-    
+
     if($scope.pullTrigger) {
       $timeout($scope.pullTrigger);
     }
-    
+
     min = $scope.min();
     while((min > $scope.total) && $scope.hasPrevious()) {
       $scope.previous();
     }
   };
-  
+
   $scope.previous = function () {
     $scope.page--;
-    
+
     if($scope.pullTrigger) {
       $timeout($scope.pullTrigger);
     }
-    
+
     min = $scope.min();
   };
+
+  $scope.first = function () {
+    $scope.page = 1;
+
+    if($scope.pullTrigger) {
+      $timeout($scope.pullTrigger);
+    }
+
+    min = $scope.min();
+  };
+
 }]);

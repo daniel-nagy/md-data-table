1ad6e8dc9571721e7a4e35173eed0a48c09375ff
Daniel Nagy <Danny@Daniels-MacBook-Pro.local>
2015-08-09 21:34:57 -0400

    soft tabs

--- dist/md-data-table.js
+++ dist/md-data-table.js
@@ -238,27 +238,27 @@ mdDataTableCtrl.$inject = ['$attrs', '$element', '$q', '$scope'];
 angular.module('md.data.table').directive('mdTableCell', mdTableCell);
 
 function mdTableCell() {
-	'use strict';
-	
-	function postLink(scope, element) {
-		var select = element.find('md-select');
-		
-		if(select.length) {
-			
-			select.on('click', function (event) {
-				event.stopPropagation();
-			});
-			
-			element.addClass('clickable').on('click', function (event) {
-				event.stopPropagation();
-				select[0].click();
-			});
-		}
-	}
-	
-	return {
-		link: postLink
-	};
+  'use strict';
+  
+  function postLink(scope, element) {
+    var select = element.find('md-select');
+    
+    if(select.length) {
+      
+      select.on('click', function (event) {
+        event.stopPropagation();
+      });
+      
+      element.addClass('clickable').on('click', function (event) {
+        event.stopPropagation();
+        select[0].click();
+      });
+    }
+  }
+  
+  return {
+    link: postLink
+  };
 }
 
 angular.module('md.data.table').directive('mdTableFoot', mdTableFoot);

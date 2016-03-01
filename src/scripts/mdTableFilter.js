d2151a8937712b175894cf26b18099382be8015f
SMxJrz <Smelecs@gmail.com>
2016-03-01 08:47:29 -0500

    Added a filter to the md-data-table toolset, this allows the table to filter on each column. The filter uses the column object to perform the checks and It will do lookups to the table data based on the orderBy attribute, (alternatively you can provide searchBy in that object if you don't want to do ordering for that particular column). This change only provides the filter for the user, how they lay it out should ultimately be up to them.

---
+++ src/scripts/mdTableFilter.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('md.data.table')
+    .filter('mdTableColumnarFilter', columnarFilter);
+
+function columnarFilter(){
+    //utility for nested strings in column orderBy / column searchBy
+    //this handles cases where the value is nested within the data object
+    //example is when the location of the column is orderBy/searchBy value "myData.obj.name"
+    var resolveValue = function(obj, prop) {
+        var _index = prop.indexOf('.');
+        if(_index > -1) {
+            return fetchFromObject(obj[prop.substring(0, _index)], prop.substr(_index + 1));
+        }
+
+        return obj[prop];
+    }
+
+
+    return function (values, columns) {
+        var result = values;
+        columns.forEach(function(col){
+            // if a column has an orderBy already use that. otherwise a searchBy can be specified.
+            var colId = col.orderBy ? col.orderBy : col.searchBy;
+            //if an orderBy or searchBy value is set and the column filter has a value, it will filter on that column
+            if(colId && col.filter){
+                //filter the current set of values
+                result = result.filter(function(row){
+                    var val = (resolveValue(row, colId));
+                    return val && val.toString().toLowerCase().indexOf(col.filter.toLowerCase()) !== -1;
+                });
+            }
+        });
+        return result;
+    };
+}

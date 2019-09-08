e74709fe444b971f354b4f0ed31afe3c7d23ca5e
Daniel Nagy <daniel_nagy@me.com>
2019-09-08 11:35:18 -0700

    fix vulnerability

--- scripts/authorizeResource.js
+++
@@ -1,5 +0,0 @@
-angular.module('nutritionApp').factory('$authorize', ['$resource', function ($resource) {
-  'use strict';
-
-  return $resource('https://infinite-earth-4803.herokuapp.com/authorize/:secret');
-}]);
--- scripts/deleteController.js
+++ scripts/deleteController.js
@@ -1,32 +1,29 @@
-angular.module('nutritionApp').controller('deleteController', ['$authorize', 'desserts', '$mdDialog', '$nutrition', '$scope', '$q', function ($authorize, desserts, $mdDialog, $nutrition, $scope, $q) {
+angular.module('nutritionApp').controller('deleteController', ['desserts', '$mdDialog', '$nutrition', '$scope', '$q', function (desserts, $mdDialog, $nutrition, $scope, $q) {
   'use strict';
-  
+
   this.cancel = $mdDialog.cancel;
-  
+
   function deleteDessert(dessert, index) {
-    var deferred = $nutrition.desserts.remove({id: dessert._id});
-    
-    deferred.$promise.then(function () {
+    var deferred = $nutrition.desserts.remove({
+      id: dessert._id,
+      secret: $scope.authorize.secret
+    });
+
+    return deferred.$promise.then(function () {
       desserts.splice(index, 1);
     });
-    
-    return deferred.$promise;
   }
-  
+
   function onComplete() {
     $mdDialog.hide();
   }
-  
+
   function error() {
     $scope.error = 'Invalid secret.';
   }
-  
-  function success() {
-    $q.all(desserts.forEach(deleteDessert)).then(onComplete);
-  }
-  
-  this.authorizeUser = function () {
-    $authorize.get({secret: $scope.authorize.secret}, success, error);
+
+  this.submit = function () {
+    $q.all(desserts.map(deleteDessert)).then(onComplete).catch(error);
   };
-  
+
 }]);
